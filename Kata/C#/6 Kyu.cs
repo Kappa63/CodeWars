bf5658287810fc84d13886933c45bf5fb4e6efc4
Karim Q <kareem.qiblawi@gmail.com>
2021-04-10 13:18:24 +0300

    Update 6 Kyu.cs

--- Kata/C#/6 Kyu.cs
+++ Kata/C#/6 Kyu.cs
@@ -4,3 +4,20 @@ public class Kata
 {
   public static int[] ArrayDiff(int[] a, int[] b)=>a.Where(val => !b.Contains(val)).ToArray();
 }
+
+
+//Good vs Evil
+using System;
+using System.Linq;
+public class Kata
+{
+  public static string GoodVsEvil(string g, string e)
+  {
+    int[] GoodWorth = {1,2,3,3,4,10}; int[] EvilWorth = {1,2,2,2,3,5,10};
+    int GoodTotal=0; int EvilTotal=0;
+    int[] Good = g.Split(" ").Select(int.Parse).ToArray(); int[] Evil = e.Split(" ").Select(int.Parse).ToArray();
+    for(int i=0;i < Good.Length;i++)GoodTotal+= GoodWorth[i]*Good[i];
+    for(int i=0;i < Evil.Length;i++)EvilTotal+= EvilWorth[i]*Evil[i];
+    return "Battle Result: " + (GoodTotal<EvilTotal?"Evil eradicates all trace of Good":GoodTotal>EvilTotal?"Good triumphs over Evil":"No victor on this battle field");
+  }
+}
