9eb30bdba3f67c1061e5a5a1686845cdef712358
Karim Q <kareem.qiblawi@gmail.com>
2021-05-16 21:07:01 +0300

    Update 6 Kyu.cs

--- C#/6 Kyu.cs
+++ C#/6 Kyu.cs
@@ -26,15 +26,39 @@ public class Kata
 //Chess Fun #1: Chess Board Cell Color
 using System;
 using System.Text;
-
 namespace myjinxin
 {   
-    public class Kata
-    {
-        public bool ChessBoardCellColor(string cell1, string cell2){
-          int H1 = Encoding.ASCII.GetBytes(cell1.Substring(0, 1))[0]; int W1 = Convert.ToInt32(cell1.Substring(1));
-          int H2 = Encoding.ASCII.GetBytes(cell2.Substring(0, 1))[0]; int W2 = Convert.ToInt32(cell2.Substring(1));
-          return (W1%2==0?(H1%2==0?true:false):(H1%2==0?false:true)) == (W2%2==0?(H2%2==0?true:false):(H2%2==0?false:true));
-        }
+  public class Kata
+  {
+    public bool ChessBoardCellColor(string cell1, string cell2){
+      int H1 = Encoding.ASCII.GetBytes(cell1.Substring(0, 1))[0]; int W1 = Convert.ToInt32(cell1.Substring(1));
+      int H2 = Encoding.ASCII.GetBytes(cell2.Substring(0, 1))[0]; int W2 = Convert.ToInt32(cell2.Substring(1));
+      return (W1%2==0?(H1%2==0?true:false):(H1%2==0?false:true)) == (W2%2==0?(H2%2==0?true:false):(H2%2==0?false:true));
     }
+  }
+}
+
+
+//Find The Parity Outlier
+using System.Collections.Generic;
+using System.Linq;
+using System;
+public class Kata
+{
+  public static int Find(int[] integers)
+  {
+    int? e = null;
+    int? o = null;
+    foreach (int i in integers){
+      if (i%2==0){
+        if(e==null)e=i;
+        else if(e!=null && o!=null)return Convert.ToInt32(o);
+      }
+      else{
+        if(o==null)o=i;
+        else if(o!=null && e!=null)return Convert.ToInt32(e);
+      } 
+    }
+    return integers[integers.Length-1];
+  }
 }
