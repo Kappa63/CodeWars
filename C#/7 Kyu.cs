a04dc7c33c98b24e7c2c569599b8c2374eeb99b8
Karim Q <kareem.qiblawi@gmail.com>
2021-05-16 20:23:24 +0300

    Update 7 Kyu.cs

--- C#/7 Kyu.cs
+++ C#/7 Kyu.cs
@@ -5,3 +5,12 @@ public class Kata
 {
   public static bool ValidatePin(string pin) => return Regex.IsMatch(pin, @"^(\d{4}|\d{6})\z");
 }
+
+
+//Printer Errors
+using System.Linq;
+using System;
+public class Printer 
+{
+    public static string PrinterError(String s)=> String.Format("{0}/{1}", s.Count(L=> !"abcdefghijklm".Contains(L)), s.Count());
+}
