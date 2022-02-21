5e8e225e91678ec8f4dc6c3f1d797987f9ac2e58
Karim Q <kareem.qiblawi@gmail.com>
2022-02-21 18:31:41 +0200

    Update 3 Kyu.py

--- Python/3 Kyu.py
+++ Python/3 Kyu.py
@@ -12,3 +12,33 @@ def spiralize(size):
             if i>2 and  A[xt+D[0]][yt+D[1]] == 1: break
             x, y = xt, yt
     return A
+
+#Binomial Expansion
+import math
+import regex as re
+def expand(expr):
+    n = re.findall("[- | -\d]+", expr)
+    p = int(n[-1])
+    y = abs(int(n[-2]))
+    try:
+        x = -1 if n[-3] == '-' else int(n[-3])
+    except: 
+        x = 1
+    v = re.findall("[a-z]+", expr)[0]
+    s = []
+    f = {1: "", -1:"-"}
+    for i in range(p+1):
+        cof = math.comb(p, i)*pow(y, i)*pow(x, p-i)
+        cof = f[cof] if cof in f else cof
+        s.append(f"{cof}{v+'^'+str(p-i) if p-i not in [1,0] else v if p-i == 1 else '1' if cof == '' else ''}")
+    if "+" in expr:
+        a = "+".join(s)
+    else:
+        a = s[0]
+        x = 1
+        for i in s[1:]:
+            a += ('-' if x else '+')+i
+            x = not x
+    a = a.replace("+-", "-");
+    a = a.replace("--", "+");
+    return a.replace("-+", "-");
